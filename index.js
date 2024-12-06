906eb4d7bdd9d071977204202c1cc3f9da75709d
Gauravkr7488 <gauravkr748800@gmail.com>
2024-12-06 17:50:44 +0530

    added POST function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
 const PORT = 8000;
+
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`)
 )
@@ -23,4 +26,23 @@ app.get("/users/api/:id", (req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
     return res.json(user);
+})
+
+app.post("/users/api", (req, res) => {
+    const body = req.body;
+    console.log(body);
+    users.push({...body, id: users.length + 1});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+
+        return res.json({ status: "pending" });
+    });
+});
+
+app.patch("/users/api/:id", (req, res) => {
+    return res.json({ status: "pending" })
+})
+
+
+app.delete("/users/api/:id", (req, res) => {
+    return res.json({ status: "pending" })
 })
